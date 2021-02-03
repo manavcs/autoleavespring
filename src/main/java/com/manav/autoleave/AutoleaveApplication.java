46be6ecdb9e4aa7eb236cc25b70e93d983a97c0b
LAP-WIN-1210\hasher <manvendra.yadav@honeywell.com>
2021-02-03 13:22:51 +0530

    initial commit

---
+++ src/main/java/com/manav/autoleave/AutoleaveApplication.java
@@ -0,0 +1,13 @@
+package com.manav.autoleave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoleaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoleaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manav/autoleave/AutoleaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manav.autoleave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoleaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
